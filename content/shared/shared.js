8e2dfb2d8d05b040fa206b8a5ad74386f064b599
Samuel S. Watson <samuel.s.watson@gmail.com>
2019-06-19 14:17:58 -0400

    Editor indentation set to 4 earlier to take effect

--- content/shared/shared.js
+++ content/shared/shared.js
@@ -11,12 +11,12 @@ import './components/quill';
 script('/resources/shared/static/prism.js');
 
 script('/resources/shared/static/juniper.min.js').then(() => {
+  if (window.CodeMirror) window.CodeMirror.defaults.indentUnit = 4;
   new window.Juniper({
     repo: 'sswatson/simple-python-stack',
     isolateCells: false,
     msgLoading: 'Loading or None returned'
   });
-  if (window.CodeMirror) window.CodeMirror.defaults.indentUnit = 4;
 });
 
 window.MathJax = {
